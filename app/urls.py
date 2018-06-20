d2bd843b87a507845282a4a043c032274ecf61cf
Christian Escalante <cescalante@indestim.es>
2018-06-20 08:22:21 +0200

    Se añade los endpoint para el proyecto

--- app/urls.py
+++ app/urls.py
@@ -1,1 +1,23 @@
-urlpatterns = []
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+from rest_framework_swagger.views import get_swagger_view
+
+from app.api import AuthorViewSet, BookViewSet
+
+VERSION_API = 'v1/'
+
+router_author = DefaultRouter()
+router_author.register(VERSION_API + 'author', AuthorViewSet, base_name='api_author_')
+
+router_book = DefaultRouter()
+router_book.register(VERSION_API + 'book', BookViewSet, base_name='api_book_')
+
+swagger_view = get_swagger_view(title='API Demo')
+
+urlpatterns = [
+    path(r'', include(router_author.urls)),
+    path(r'', include(router_book.urls)),
+
+    path(VERSION_API + 'doc', swagger_view),
+]

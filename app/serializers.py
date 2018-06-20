4d11d2cde0c826bfd761a6e6c91340fa95d28199
Christian Escalante <cescalante@indestim.es>
2018-06-20 08:13:13 +0200

    Se añade un fichero de serializador de los modelos

---
+++ app/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from app.models import Author, Book
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        exclude = []
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        exclude = []

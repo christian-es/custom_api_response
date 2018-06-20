0c4b05f3124f69cd12262daf2a0a6c6a3e12a69e
Christian Escalante <cescalante@indestim.es>
2018-06-20 08:11:05 +0200

    Se añade los modelos y las migraciones

--- app/models.py
+++ app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=120)
+    alias = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=120)
+    pages = models.IntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
